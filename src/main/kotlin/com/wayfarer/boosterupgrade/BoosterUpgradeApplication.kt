e6e3e2d48a0ac0d5983795e45aff3a4439e1e2ca
Robert Tieber <robert.tieber@gmail.com>
2021-05-28 10:59:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/wayfarer/boosterupgrade/BoosterUpgradeApplication.kt
@@ -0,0 +1,11 @@
+package com.wayfarer.boosterupgrade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoosterUpgradeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoosterUpgradeApplication>(*args)
+}
---
+++ src/test/kotlin/com/wayfarer/boosterupgrade/BoosterUpgradeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wayfarer.boosterupgrade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoosterUpgradeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
